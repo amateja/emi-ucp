f2d577a89bc027bbd3549d8e2d6a8eedf735716b
Andrzej Mateja <mateja.and@gmail.com>
2016-02-18 21:47:30 +0100

    Oh well... Python 2.6 and 3.2 is not supported. Need to do more checks.

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
                  'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Topic :: Communications :: Telephony'
